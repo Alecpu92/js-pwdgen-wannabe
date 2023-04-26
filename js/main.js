f628cefd1914dc4dbcbbf66a473bf09b514baa7f
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-04-26 16:55:00 +0200

    esercitazione js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// chiedi all'utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito. Infine scrivi sulla pagina nomecognomecolorepreferito21
+// chiedere all'utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito. Infine scrivi sulla pagina nomecognomecolorepreferito21
 
 // chiedere nome utente
 
